852ce51146406c2ca2087353ee412a005b93ffbe
maheshhbalwan <85355816+maheshhbalwan@users.noreply.github.com>
2023-05-08 22:36:39 +0530

    feat: display transactions

--- server/index.js
+++ server/index.js
@@ -50,6 +50,12 @@ app.post("/api/transaction", async (req, res) => {
   }
 });
 
+app.get("/api/transaction", async (req, res) => {
+  await mongoose.connect(process.env.MONGO_URL);
+  const transactions = await Transaction.find();
+  res.json(transactions);
+});
+
 // Static Port
 app.listen(port);
 console.log(`App listening on port http://localhost:${port}${url}`);
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [name, setName] = useState("");
   const [datetime, setDatetime] = useState("");
   const [description, setDescription] = useState("");
+  const [transactions, setTransactions] = useState("");
+
+  useEffect(() => {
+    getTransactions().then(setTransactions);
+  }, []);
+
+  async function getTransactions() {
+    // const url = (process.env.REACT_APP_API_URL + "/transaction");
+    const url = "http://localhost:4000/api" + "/transaction";
+    const response = await fetch(url);
+    return await response.json();
+  }
 
   function addNewTransaction(e) {
     e.preventDefault();
@@ -32,15 +44,25 @@ function App() {
     });
   }
 
+  let balance = 0;
+  for (const transaction of transactions) {
+    balance = balance + transaction.price;
+  }
+  balance = balance.toFixed(2);
+
   return (
     <main>
+      <h1>
+        {balance}
+        <span>.00</span>
+      </h1>
       <form action="" onSubmit={addNewTransaction}>
         <div className="basic">
           <input
             type="text"
             value={name}
             onChange={(e) => setName(e.target.value)}
-            placeholder={"name"}
+            placeholder={"price and name"}
           />
           <input
             value={datetime}
@@ -59,36 +81,26 @@ function App() {
         <button type="submit">Add new transaction</button>
       </form>
       <div className="transactions">
-        <div className="transaction">
-          <div className="left">
-            <div className="name">New Samsung TV</div>
-            <div className="description">It was time for new TV</div>
-          </div>
-          <div className="right">
-            <div className="price red">-$500</div>
-            <div className="datetime">2023-05-05 15:45</div>
-          </div>
-        </div>
-        <div className="transaction">
-          <div className="left">
-            <div className="name">New Samsung TV</div>
-            <div className="description">It was time for new TV</div>
-          </div>
-          <div className="right">
-            <div className="price red">-$500</div>
-            <div className="datetime">2023-05-06 15:45</div>
-          </div>
-        </div>
-        <div className="transaction">
-          <div className="left">
-            <div className="name">Commission income</div>
-            <div className="description">from clients</div>
-          </div>
-          <div className="right">
-            <div className="price green">$400</div>
-            <div className="datetime">2023-05-07 15:45</div>
-          </div>
-        </div>
+        {transactions.length > 0 &&
+          transactions.map((transaction, index) => (
+            <div key={index} className="transaction">
+              <div className="left">
+                <div className="name">{transaction.name}</div>
+                <div className="description">{transaction.description}</div>
+              </div>
+              <div className="right">
+                {/* console.log(transaction.price); */}
+                <div
+                  className={
+                    "price " + (transaction.price < 0 ? "red" : "green")
+                  }
+                >
+                  {transaction.price}
+                </div>
+                <div className="datetime">{transaction.datetime}</div>
+              </div>
+            </div>
+          ))}
       </div>
     </main>
   );
